72481e2079f1801d12106e8c9742c29cd1ea09b0
SHREYA SHARMA <90905061+shreyasharma-1@users.noreply.github.com>
2025-07-28 01:25:44 +0530

    Add files via upload

---
+++ Ride_on_miles/Project1.py
@@ -0,0 +1,26 @@
+"""
+Project: 1
+
+Create a python program that asks the user how far they want to travel. If they want to travel less than
+
+three miles tell them to ride Bicycle. If they want to travel more than three miles, but less than three
+
+hundred miles, tell them to ride Motor-Cycle. If they want to travel three hundred miles or more tell them to
+
+driver Super-Car.
+
+Sample Output:
+
+How far would you like to travel in miles? 2500
+
+I suggest Super-Car to your destination
+"""
+
+distance = int(input("How far would you like to travel in miles? "))
+
+if distance < 3:
+    print("I suggest Bicycle to your destination")
+elif distance < 300:
+    print("I suggest Motor-Cycle to your destination")
+else:
+    print("I suggest Super-Car to your destination")
